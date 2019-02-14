b04cbcf0fefdc30b37d662b72da507c985c33e8b
crinamr <crina.renciuc@gmail.com>
2019-02-14 19:30:50 +0100

    New

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Welcome to Conference Website' });
+  res.render('index', {page:'Home', menuId:'home'});
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,28 @@ router.get('/', function(req, res, next) {
   res.send('respond with a reùsource');
 });
 
+router.get('/register', function(req, res, next) {
+  res.render('register', {title: 'Register'});
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login', {title: 'Login'});
+});
+
+router.post('/login', function(req, res, next) {
+  res.render('login', {title: 'Login'});
+});
+
+router.get('/register', function(req, res, next) {
+  var name = req.body.name;
+  var email = req.body.email;
+  var username = req.body.username;
+  var password = req.body.password;
+  
+  res.render('login', {title: 'Login'});
+});
+
+
+
+
 module.exports = router;
